30dbab1c35205758a3dc398a6dcdb816882fbb79
Lewis Kimber <lewis.kimber7@gmail.com>
2017-05-21 14:05:20 +0000

    Can now assign leaves to result of asynchronous actions

--- loadDir.js
+++ loadDir.js
@@ -1,7 +1,7 @@
 const fs    = require("fs")
     , path  = require("path")
 
-function loadDir(dir) {
+function loadDir(dir, promise) {
   return new Promise((pass, fail) => {
     const acc = {}
     const errors = []
@@ -42,8 +42,20 @@ function loadDir(dir) {
                   JSON[file] = {}
                   loadDir(path.join(dir, file), JSON[file])
                 } else {
-                  JSON[file.slice(0, -5)] = file
-                  barrier.hit()
+                  if(promise) {
+                    promise(filePath)
+                      .then((result) => {
+                        JSON[file.slice(0, -5)] = result
+                        barrier.hit()
+                      })
+                      .catch((err) => {
+                        errors.push("Error executing file: "+err)
+                        barrier.hit()
+                      })
+                  } else {
+                    JSON[file.slice(0, -5)] = file
+                    barrier.hit()
+                  }
                 }
               }
             })
@@ -57,6 +69,27 @@ function loadDir(dir) {
 }
 
 loadDir("jsql")
+  .then((JSON) => {
+    console.log("Finished:", JSON)
+  })
+  .catch((err) => {
+    console.error("Failed to load directory with:", err)
+  })
+
+let readFile = function(file) {
+  return new Promise((pass, fail) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if(err) {
+        fail(err)
+      } else {
+        let result = data
+        pass(result)
+      }
+    })
+  })
+}
+
+loadDir("jsql", readFile)
   .then((JSON) => {
     console.log("Finished:", JSON)
   })
